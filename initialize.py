9c1130fde82bb8a56b9c4042174677e97c5bf73c
Andrew Scala <agscala@gmail.com>
2013-03-02 17:36:33 -0500

    Updating initialize for reservations

--- initialize.py
+++ initialize.py
@@ -1,12 +1,22 @@
 import sqlite3
 
 def create_tables():
-	conn = sqlite3.connect("portal.db")
+	conn = sqlite3.connect("reservations.db")
 	cursor = conn.cursor()
-	cursor.execute("CREATE TABLE links(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, url TEXT)");
+
+	cursor.execute("""
+		CREATE TABLE reservations (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			tag TEXT DEFAULT "",
+			handle TEXT UNIQUE NOT NULL,
+			email TEXT UNIQUE NOT NULL,
+			timestamp TEXT
+		) """)
+
 	conn.commit()
 	cursor.close()
 
 
 if __name__ == "__main__":
 	create_tables()
+
